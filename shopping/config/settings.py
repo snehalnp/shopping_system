f4a484db15fb357085f693d2eadc643e0033485b
snehalnp <snehalpalve1998@gmail.com>
2024-04-09 17:50:47 +0530

    databases changes for deployment

--- shopping/config/settings.py
+++ shopping/config/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'shoppingApp',
         'USER': 'postgres',
         'PASSWORD': 'mysecretpassword',
-        'HOST': 'localhost',
+        'HOST': 'database-1.cvioakq8we4f.eu-north-1.rds.amazonaws.com',
         'PORT': '5432',
     }
 }

2ac0f53722c929e647a84461b55e95f6f7d564d5
snehalnp <snehalpalve1998@gmail.com>
2024-05-07 16:18:42 +0530

    changes for allowed host

--- shopping/config/settings.py
+++ shopping/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--w%(@^+!y92ah*tl2)d00n0mo$$uk6^p*j8fg6ywxxy=%zd-3^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = '/static/'
 
 
